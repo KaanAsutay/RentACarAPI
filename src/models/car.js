82c132cbd2d49382fbbd60b6d9460802a3d80293
Kaan <kaanasutay10@gmail.com>
2024-03-10 17:40:58 +0300

    carModels finish

--- src/models/car.js
+++ src/models/car.js
@@ -31,3 +31,62 @@ const { mongoose } = require('../configs/dbConnection')
 }
 /* ------------------------------------------------------- */
 // Car Model:
+
+const CarSchema = new mongoose.Schema({
+
+    plateNumber: {
+        type: String,
+        trim: true,
+        required: true,
+        unique: true
+    },
+
+    brand: {
+        type: String,
+        trim: true,
+        required: true,
+    },
+
+    model: {
+        type: String,
+        trim: true,
+        required: true,
+    },
+
+    year: {
+        type: Number,
+        required: true,
+        min: 2000
+    },
+
+    isAutomatic: {
+        type: Boolean,
+        default: false
+    },
+
+    pricePerDay: {
+        type: Number,
+        required: true,
+    },
+
+    isPublish: {
+        type: Boolean,
+        default: true
+    },
+
+    createdId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+
+    updatedId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    }
+
+}, {collection: 'cars', timestamps: true})
+
+/* ------------------------------------------------------- */
+module.exports = mongoose.model('Car', CarSchema)
