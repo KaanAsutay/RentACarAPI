d008d631d514a2f704d6d1b45a67739dc0b8fd2a
Kaan <kaanasutay10@gmail.com>
2024-03-10 17:44:24 +0300

    reservationModels finish

--- src/models/reservation.js
+++ src/models/reservation.js
@@ -18,3 +18,32 @@ const { mongoose } = require('../configs/dbConnection')
 }
 /* ------------------------------------------------------- */
 // Reservation Model:
+
+const ReservationSchema = new mongoose.Schema({
+
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+
+    carId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Car',
+        required: true,
+    },
+
+    startDate: {
+        type: Date,
+        required: true,
+    },
+
+    endDate: {
+        type: Date,
+        required: true,
+    },
+
+}, { collection: 'reservations', timestamps: true })
+
+/* ------------------------------------------------------- */
+module.exports = mongoose.model('Reservation', ReservationSchema)
