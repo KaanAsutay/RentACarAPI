2bc081529046a51b78209fe9f35054fa409ce879
Kaan <kaanasutay10@gmail.com>
2024-03-10 19:20:34 +0300

    authController create

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -5,3 +5,13 @@
 const router = require('express').Router()
 /* ------------------------------------------------------- */
 // routes/auth:
+
+const auth = require('../controllers/auth')
+
+// URL: /auth
+
+router.post('/login', auth.login)
+router.get('/logout', auth.logout)
+
+/* ------------------------------------------------------- */
+module.exports = router
