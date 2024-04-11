1e54e2e7000d2c183f2276be5c5736305072e746
Kaan <kaanasutay10@gmail.com>
2024-04-12 00:50:02 +0300

    GoogleMail (gmail)

--- index.js
+++ index.js
@@ -57,28 +57,61 @@ const nodemailer = require('nodemailer')
 }
 */
 
-// Connection to mailServer:
+// // Connection to mailServer:
+// const transporter = nodemailer.createTransport({
+//     host: 'smtp.ethereal.email',
+//     port: 587,
+//     secure: false, // false | 'tls' | 'ssl'
+//     auth: {
+//         user: 'lpvamvjr6k4cdzi7@ethereal.email',
+//         pass: 'RfcqfdMZWgNpKEh54a'
+//     }
+// })
+
+// // SendMail:
+// transporter.sendMail({
+//     from: 'lpvamvjr6k4cdzi7@ethereal.email',
+//     to: 'kaanasutay10@gmail.com', // 'abc@gmail.com, def@mail.com' // require
+//     subject: 'hello', // require
+//     text: 'Hello There your success..', 
+//     html: '<b>Hello There </b>' // require
+// }, (error, successInfo) => {
+//     if (error) console.log(error)
+//     else console.log(successInfo)
+// })
+
+// GoogleMail (gmail):
+// Google -> AccountHome -> Security -> Two-Step-Verify -> App-Passwords
+const mailSettings = {
+    service: 'Gmail',
+    user: 'kaanasutay10@gmail.com',
+    pass: 'cpnv ojeq pjep sgka' // https://myaccount.google.com/u/1/apppasswords
+}
+
+// Mail Subject/Content:
+const emailContent = {
+    from: mailSettings.user,
+    to: 'kaanasutay10@gmail.com',
+    subject: 'Hello',
+    html: '<b>Hello</b> How are you?'
+
+}
+
+// Connect to mailServer:
 const transporter = nodemailer.createTransport({
-    host: 'smtp.ethereal.email',
-    port: 587,
-    secure: false, // false | 'tls' | 'ssl'
+    service: mailSettings.service,
     auth: {
-        user: 'lpvamvjr6k4cdzi7@ethereal.email',
-        pass: 'RfcqfdMZWgNpKEh54a'
+        user: mailSettings.user,
+        pass: mailSettings.pass
     }
 })
 
 // SendMail:
-transporter.sendMail({
-    from: 'lpvamvjr6k4cdzi7@ethereal.email',
-    to: 'kaanasutay10@gmail.com', // 'abc@gmail.com, def@mail.com' // require
-    subject: 'hello', // require
-    text: 'Hello There your success..', 
-    html: '<b>Hello There </b>' // require
-}, (error, successInfo) => {
-    if (error) console.log(error)
-    else console.log(successInfo)
+transporter.sendMail(emailContent, (error, info) => {
+    error ? console.log(error) : console.log(info)
 })
+
+
 
 /* ------------------------------------------------------- */
 // Routes:
